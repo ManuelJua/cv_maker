65a234a0a8db846124f51f91814aa1e0c699f441
ManuelJua <manuelalejandrojuarez@gmail.com>
2025-11-16 02:22:22 +0000

    Add staging frontend url to allowed CORS

--- backend/main.py
+++ backend/main.py
@@ -25,6 +25,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://cvmakerfrontend-production.up.railway.app",
+                   "https://cvmakerfrontend-staging.up.railway.app",
         "http://localhost:3000"],  # for local development 
     allow_credentials=True,
     allow_methods=["*"],
