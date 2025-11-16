d8afbd9add3a46e6bfc5a9660a83611c3d68bda6
ManuelJua <manuelalejandrojuarez@gmail.com>
2025-11-16 02:19:02 +0000

    Change to stagin api url

--- frontend/js/config/config.js
+++ frontend/js/config/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    API_BASE_URL: `http://localhost:8080/api`,
+    API_BASE_URL: `https://cvmakerbackend-staging.up.railway.app/api`,
     ALLOWED_FILE_TYPES: ['application/pdf', 'text/plain'],
     ALLOWED_DOMAINS: ['linkedin.com', 'indeed.com', 'reed.co.uk'],
     MAX_FILE_SIZE: 10 * 1024 * 1024, // 10MB
