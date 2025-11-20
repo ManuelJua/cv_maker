4ee0ad96c85f194a6480b481135a01eff9019cc3
ManuelJua <manuelalejandrojuarez@gmail.com>
2025-11-20 16:47:17 +0000

    change CORS

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://cvmakerfrontend-production.up.railway.app",
-                   os.getenv("STAGING_FRONTEND_URL"),
+                   "https://cvmakerfrontend-staging.up.railway.app",
         "http://localhost:3000"],  # for local development 
     allow_credentials=True,
     allow_methods=["*"],
